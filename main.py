08ba8f0c091b452cbb12a86312fdbfbd945f2ee1
EliFrankel2 <>
2022-09-21 17:19:46 +0000

    commit message

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return render_template("index.html")
-
+@app.route("/")
+def welcome():
+  return render_template("welcome_page.html")
 
 app.run(debug=True, host='0.0.0.0')
 
